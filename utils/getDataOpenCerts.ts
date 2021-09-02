4a03f783aece8b2899a10789cc82a76fe160c9a7
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-09-02 10:05:07 +0800

    chore: add console.warn message

--- utils/getDataOpenCerts.ts
+++ utils/getDataOpenCerts.ts
@@ -4,8 +4,10 @@ import { V2OrV3WrappedDocument } from "../types";
 export const getDataV2OrV3 = (wrappedDocument: V2OrV3WrappedDocument) => {
   if (utils.isWrappedV2Document(wrappedDocument)) return getData(wrappedDocument);
   else if (utils.isWrappedV3Document(wrappedDocument)) return wrappedDocument;
-  return wrappedDocument;
-  // else throw new Error("Unknown type of document.");
+  else {
+    console.warn("Unknown V2 or V3 OA document type.");
+    return wrappedDocument;
+  }
 };
 
 export const getTemplateUrl = (wrappedDocument: V2OrV3WrappedDocument) => {
