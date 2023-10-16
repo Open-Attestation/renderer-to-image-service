1115d79821c52d4d3d7e9a61dc87a8783111ff34
Kyle Huang Junyuan <kyle@hjunyuan.com>
2023-10-16 09:58:39 +0800

    chore: remove old sample

--- fixtures/online-samples.ts
+++ fixtures/online-samples.ts
@@ -12,17 +12,10 @@ export const sampleCerts: {
   {
     title: "[Encrypted] OpenCerts Demo",
     q: { payload: { uri: "https://gallery.openattestation.com/static/documents/transcript-encrypted.opencert" } },
-    anchor: { key: "5b433c297f3b35690461b9ee08d77f3e8ee47ec86e5b8b1322b056da6f0b86c4" },
+    anchor: { key: "691add1930798b63b17c8683a4776bedc16771ea5664337e21a563be0529024f" },
   },
   {
     title: "[Plaintext] PDT HealthCert v2.0",
     q: { payload: { uri: "https://schemata.openattestation.com/sg/gov/moh/pdt-healthcert/2.0/endorsed-wrapped.json" } },
-  },
-  {
-    title: "[Encrypted] PDT HealthCert v2.0",
-    q: {
-      payload: { uri: "https://api-vaccine.storage.staging.notarise.io/document/44409930-70b6-4ab7-94be-86cf57c9ed87" },
-    },
-    anchor: { key: "52ae6dafd2ce6f818ccb7d893f4fb58416453f42c4624a9c6a98abf0760f2119" },
   },
 ];
